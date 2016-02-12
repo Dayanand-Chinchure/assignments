03c1799d4d2c7dcd8f7638cb989b331d6cfbe28b
Dayanand-Chinchure <dchinchure@gmail.com>
2016-02-12 02:32:46 -0500

    My-Assignments

--- epl/assignment-1/problem4_a.scm
+++ epl/assignment-1/problem4_a.scm
@@ -1,14 +1,10 @@
-(define c 0)
-
 (define gen
   (lambda (n)
-    (if (= c n) 0
-      ((append (list (+ c 1)) (gen (- n 1)))))))
+    (if (= n 0) '()
+      (cons n (gen (- n 1))))))
 
 (define do-calc
-  (lambda (n gen)
-    (if (null? gen) 0
-      (if (= n 0) 0 
-	(+ (car gen) (do-calc (- n 1) (cdr gen))))))
-  (gen n)
+  (lambda (n)
+    (if (= n 0) 0
+	(+ (car (gen n)) (do-calc (- n 1)))))
  )
