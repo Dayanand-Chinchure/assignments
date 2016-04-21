898884f4b9f76e120dff3e4732ac2cb65be3a834
Dayanand-Chinchure <dchinchure@gmail.com>
2016-04-21 11:35:25 +0530

    updated 21-04-16

--- epl/assignment-4/assign4.scm
+++ epl/assignment-4/assign4.scm
@@ -1,13 +1,25 @@
-(define parse-expression
-  (lambda (datum)
-    (cond
-      ((symbol? datum) (var-exp datum))
-      ((pair? datum)
-       (if (eqv? (car datum) 'lambda)
-         (lambda-exp (caadr datum)
-           (parse-expression (caddr datum)))
-         (app-exp
-           (parse-expression (car datum))
-           (parse-expression (cadr datum)))))
-      (else (eopl:error 'parse-expression
-              "Invalid concrete syntax ~s" datum)))))
+(define do-parse
+  (lambda (lst lst1)
+    (if (null? lst) lst1
+      (list 'lambda (car lst) (do-parse (cdr lst) lst1)))))
+
+(define parse
+  (lambda (lst)
+    (if (null? lst) lst
+      (if (eq? (car lst) 'lambda) (do-parse (cadr lst) (caddr lst)) '()))))
+
+(define sym '(+ - / *))
+
+(define check-symbol
+  (lambda (a sym)
+    (if (null? sym) #f 
+      (if (eq? (car sym) a) #t (check-symbol a (cdr sym))))))
+
+(define ast
+  (lambda (lst)
+    (if (null? lst) lst
+      (cond
+	((eq? (car lst) 'lambda) (list 'abs (ast (cdr lst))))
+	((symbol? (car lst)) (if (check-symbol (car lst) sym) (list 'num-op (car lst) (ast (cdr lst))) 
+			       (list 'var-ref (car lst) (ast (cdr lst)))))
+	((list? (car lst)) (ast (car lst)))))))
---
+++ epl/assignment-4/programs.scm
@@ -0,0 +1,135 @@
+;(load "../assignment-3/programs.scm")
+
+(define normal-add
+  (lambda (x y)
+    (+ x y)))
+
+(define curried-add
+  (lambda (x)
+    (lambda (y)
+      (+ x y))))
+
+;(normal-add 3 5)
+
+;; written this because i was not able to pass `and` as parameter to myfold
+(define my-and
+  (lambda (a b)
+    (and a b)))
+
+(define myfold
+  (lambda (function accumulator lst)
+    (if (null? lst) accumulator
+      (myfold function (function accumulator (car lst)) (cdr lst)))))
+
+
+(define is-expression-grammatical-correct?
+  (lambda (expression)
+     (let ((lst-operators '(+ *)))
+      (cond
+        ((null? expression)                      #f)
+        
+        ((symbol? expression)                    #t)
+
+        ((number? expression)                    (not (negative? expression)))
+                                                 
+                                                 ;; check if parameters are symbols only  &
+        ;; abstraction                           ;; the body of abstraction is grammatically correct
+        ((and (= (length expression) 3) 
+              (eq? 'lambda (car expression)))    (and (myfold my-and #t (map symbol? (cadr expression)))
+                                                      (is-expression-grammatical-correct? (caddr expression))))
+      
+        ;; num-op
+        ((elem? (car expression) lst-operators)  (if (not (= (length (cdr expression)) 2)) #f
+                                                      (and (is-expression-grammatical-correct? (cadr expression))
+                                                           (is-expression-grammatical-correct? (caddr expression)))))
+
+        
+        ;; application
+        ((= (length expression) 2)               (and (is-expression-grammatical-correct? (car expression))
+                                                      (is-expression-grammatical-correct? (cadr expression))))
+        
+        (else                                    #f)))))
+
+        
+
+(define convert-lambda-abs-to-curried-form
+  (lambda (expression lst-bound-vars)
+    (if (null? lst-bound-vars) (caddr expression)
+      (append (cons 'lambda
+                    (list (car lst-bound-vars)))
+              (list (convert-lambda-abs-to-curried-form expression (cdr lst-bound-vars)))))))
+
+
+
+(define convert-to-curried-form
+  (lambda (expression)
+    (cond 
+      ((symbol? expression)              expression)
+      
+      ((number? expression)              expression)
+      
+      ((eq? 'lambda (car expression))    (convert-lambda-abs-to-curried-form expression 
+                                                                            (cadr expression)))
+      
+      ((or (eq? '+ (car expression))
+           (eq? '* (car expression)))    (append (list (car expression))
+                                                 (convert-to-curried-form (cadr expression))
+                                                 (convert-to-curried-form (caddr expression))))
+      
+      (else                              (append (list (convert-to-curried-form (car expression)))
+                                                 (list (convert-to-curried-form (cadr expression))))))))
+
+
+(define return-parse-tree
+  (lambda (expression)
+    (let ((lst-operators '(+ *)))
+     (cond
+       ((symbol? expression)                    (cons 'var-exp
+                                                      (list expression)))
+
+       ((number? expression)                    (cons 'number 
+                                                      (list expression)))
+
+       ;; abstraction
+       ((eq? 'lambda (car expression))          (append (list 'abs)
+                                                        (list (return-parse-tree (cadr expression))
+                                                              (return-parse-tree (caddr expression)))))
+
+       ;; if <num-op>
+       ((elem? (car expression) lst-operators)  (cons 'num-op 
+                                                      (list (car expression)
+                                                            (return-parse-tree (cadr expression))
+                                                            (return-parse-tree (caddr expression)))))
+
+
+       ;; application
+       (else                                    (append (list 'application)
+                                                        (list (return-parse-tree (car expression))
+                                                              (return-parse-tree (cadr expression)))))))))
+
+
+(define elem? 
+  (lambda (a lst)
+    (if (null? lst)
+      (if (eq? a (car lst)) #t (elem? a (cdr lst))))))
+  
+(define ll '(abs (var-exp x) (abs (var-exp y) (num-op + (var-exp x) (var-exp y)))))
+
+(define check-free 
+  (lambda (var lst)    
+     (cond
+       				((eq? (car lst) 'var-exp) (eq? var (cadr lst))) 
+				((eq? (car lst) 'abs) (and (not (eq? var (cadadr lst))) (check-free var (caddr lst))))
+				((eq? (car lst) 'num-op) (or (check-free var (cadr lst)) (check-free var (caddr lst))
+	)) 
+				(else (or (check-free var (cadr lst)) (check-free var (caddr lst))))     
+       )))
+
+(define parse
+  (lambda (expression)
+    (if (is-expression-grammatical-correct? expression) 
+      (return-parse-tree (convert-to-curried-form expression))
+      #f)))
+
+
+
